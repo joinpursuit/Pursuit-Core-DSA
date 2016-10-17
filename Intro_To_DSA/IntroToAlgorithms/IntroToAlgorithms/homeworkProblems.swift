83c83fabc196237a696af50468943f904889d3db
Victor Zhong <viczhong@gmail.com>
2016-10-17 19:09:21 -0400

    Homework submit

--- Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
+++ Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
@@ -15,31 +15,57 @@ import Foundation
 
 
 func posZeroNeg(myArr: [Int]) -> (Int, Int, Int) {
-    return (0,0,0)
+var placeholderTuple = (0, 0, 0)
+	for num in myArr {
+		switch num {
+		case _ where num < 0: placeholderTuple.0 += 1
+		case 0: placeholderTuple.1 += 1
+		case _ where num > 0: placeholderTuple.2 += 1
+		default: break
+		}
+	}
+	
+	return placeholderTuple
 }
 
 
 //Given an array of Ints, return the second smallest integer.  Assume every value is unique.  Return nil if the array has less than two elements
 
 func secondSmallest(myArr: [Int]) -> Int? {
-    return nil
+	guard myArr.count >= 2 else { return nil }
+	let myNewArr = myArr.sorted()
+	return myNewArr[1]
 }
 
 //A number is prime if it is only evenly divisible by 1 and itself.  Write an algorithm that checks if an Int is prime
 
-func isPrime(x: Int) -> Bool {
-    return true
+func isPrime(withIf x: Int) -> Bool {
+	for all in 2..<x {
+		if x % all == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 //Write an anglorithm that removes all characters from a String matching an input character
 
 func removeCharacter(s: String, c: Character) -> String {
-    return ""
+	return String(s.characters.filter({ $0 != c }))
 }
 
 //Write an algorithm that removes all characters from a String contained within an input array of characters
 
 func removeMultipleCharacters(s: String, arr: [Character]) -> String {
-    return ""
+	var newString = String()
+	outer: for value in s.characters {
+		for x in arr {
+			if x == value {
+				continue outer
+			}
+		}
+		newString += String(value)
+	}
+	return newString
 }
 
