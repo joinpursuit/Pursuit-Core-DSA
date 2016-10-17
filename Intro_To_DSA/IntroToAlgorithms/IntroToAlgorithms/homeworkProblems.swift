e0866910fb83db480d17ce1f8548a159b8baaa7e
Eric Chang <echang25@hotmail.com>
2016-10-17 19:30:33 -0400

    algorithm homework

--- Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
+++ Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
@@ -10,36 +10,72 @@ import Foundation
 
 //Given an array of Ints, return a tuple where:
 //The first element is the number of negative values
-//The second element is the number of zeros 
+//The second element is the number of zeros
 //The third element is the number of positive values.
 
 
 func posZeroNeg(myArr: [Int]) -> (Int, Int, Int) {
-    return (0,0,0)
+  let a = myArr.filter{ $0 < 0 }.count
+  let b = myArr.filter{ $0 == 0 }.count
+  let c = myArr.filter{ $0 > 0 }.count
+  return (a,b,c)
 }
 
 
 //Given an array of Ints, return the second smallest integer.  Assume every value is unique.  Return nil if the array has less than two elements
 
 func secondSmallest(myArr: [Int]) -> Int? {
-    return nil
+  guard myArr.count > 1 else { return nil }
+  return myArr.sorted()[1]
 }
 
 //A number is prime if it is only evenly divisible by 1 and itself.  Write an algorithm that checks if an Int is prime
 
 func isPrime(x: Int) -> Bool {
+  if x <= 1 {
+    return false
+  }
+  if x <= 3 {
     return true
+  }
+  var i = 2
+  while i*i <= x {
+    if x % i == 0 {
+      return false
+    }
+    i += 1
+  }
+  return true
 }
 
 //Write an anglorithm that removes all characters from a String matching an input character
 
 func removeCharacter(s: String, c: Character) -> String {
-    return ""
+  var returnThis = ""
+  for char in s.characters {
+    if char != c {
+      returnThis.append(char)
+    }
+  }
+  return returnThis
 }
 
 //Write an algorithm that removes all characters from a String contained within an input array of characters
 
 func removeMultipleCharacters(s: String, arr: [Character]) -> String {
-    return ""
+  var buffer = ""
+  var returningString = ""
+  for i in arr {
+    if !buffer.contains(String(i)) {
+      buffer.append(i)
+    }
+  }
+  
+  for i in s.characters {
+    if !buffer.contains(String(i)) {
+      returningString.append(i)
+    }
+  }
+  return returningString
 }
 
