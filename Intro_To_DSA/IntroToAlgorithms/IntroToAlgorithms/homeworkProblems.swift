58f199bf86b8fb716c36c831b677f9e2b4be7053
Kadell <Kadell@MacBook.local>
2016-10-17 21:08:08 -0400

    Finished HW

--- Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
+++ Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
@@ -15,31 +15,71 @@ import Foundation
 
 
 func posZeroNeg(myArr: [Int]) -> (Int, Int, Int) {
-    return (0,0,0)
+    var x = 0
+    var y = 0
+    var z = 0
+    for num in myArr {
+        if num < 0 {
+            x += 1
+        } else if num == 0 {
+            y += 1
+        } else {
+            z += 1
+        }
+    }
+    return (x,y,z)
 }
+
 
 
 //Given an array of Ints, return the second smallest integer.  Assume every value is unique.  Return nil if the array has less than two elements
 
 func secondSmallest(myArr: [Int]) -> Int? {
+    var sorted = myArr.sorted{$0 < $1}
+    if sorted.count > 2 {
+        return sorted[1]
+    }
+    
     return nil
 }
 
 //A number is prime if it is only evenly divisible by 1 and itself.  Write an algorithm that checks if an Int is prime
 
 func isPrime(x: Int) -> Bool {
+    let denominator = [2,3,4,5,7]
+    for i in denominator {
+        if x % i == 0 {
+        return false
+        }
+    }
     return true
 }
 
 //Write an anglorithm that removes all characters from a String matching an input character
 
 func removeCharacter(s: String, c: Character) -> String {
-    return ""
+//    var newString: String = ""
+//    for x in s.characters {
+//        if x != c {
+//        newString.append(x)
+//        }
+//    }
+//    
+//    return newString
+    let char = s.characters.filter{$0 != c }
+    return String(char)
 }
 
 //Write an algorithm that removes all characters from a String contained within an input array of characters
 
 func removeMultipleCharacters(s: String, arr: [Character]) -> String {
-    return ""
-}
+    var newString: String = ""
 
+    for char in s.characters {
+        if !arr.contains(char) {
+        newString.append(char)
+        }
+        }
+    return newString
+  
+}
