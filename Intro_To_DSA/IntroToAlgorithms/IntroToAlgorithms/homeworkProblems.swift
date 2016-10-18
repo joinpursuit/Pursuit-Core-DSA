c360c52297daa8673a61e5df7a3aa7cbd546275b
harichandansingh <singh.harichandan@gmail.com>
2016-10-17 21:58:41 -0400

    HW

--- Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
+++ Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
@@ -15,31 +15,83 @@ import Foundation
 
 
 func posZeroNeg(myArr: [Int]) -> (Int, Int, Int) {
-    return (0,0,0)
+    var numberOfNegatives = 0
+    var numberOfZeroes = 0
+    var numberOfPositives = 0
+    
+    for i in myArr {
+        if i < 0 {
+            numberOfNegatives += 1
+        }
+        else if i == 0 {
+            numberOfZeroes += 1
+        }
+        else if i > 0 {
+            numberOfPositives += 1
+        }
+    }
+    return (numberOfNegatives, numberOfZeroes, numberOfPositives)
 }
 
 
 //Given an array of Ints, return the second smallest integer.  Assume every value is unique.  Return nil if the array has less than two elements
 
 func secondSmallest(myArr: [Int]) -> Int? {
-    return nil
+    guard myArr.count >= 2 else { return nil }
+    var smallestElement = myArr[0]
+    var secondSmallestElement = myArr[1]
+    for i in myArr {
+        if i < smallestElement {
+            secondSmallestElement = smallestElement
+            smallestElement = i
+        }
+        else if i != smallestElement && i < secondSmallestElement {
+            secondSmallestElement = i
+        }
+    }
+    return secondSmallestElement
 }
 
 //A number is prime if it is only evenly divisible by 1 and itself.  Write an algorithm that checks if an Int is prime
 
 func isPrime(x: Int) -> Bool {
+    for i in 2..<x {
+        if x % i == 0 {
+            return false
+        }
+    }
     return true
 }
 
-//Write an anglorithm that removes all characters from a String matching an input character
+//Write an algorithm that removes all characters from a String matching an input character
 
 func removeCharacter(s: String, c: Character) -> String {
-    return ""
+    var outputString = String()
+    let stringArray = s.characters.map{ String($0) }
+    for i in stringArray {
+        if i != String(c) {
+            outputString += i
+        }
+        else {
+            continue
+        }
+    }
+    return outputString
 }
 
 //Write an algorithm that removes all characters from a String contained within an input array of characters
 
 func removeMultipleCharacters(s: String, arr: [Character]) -> String {
-    return ""
+    var outputString = String()
+    let stringArray = s.characters.map{ String($0) }
+    for i in stringArray {
+        if arr.contains(Character(i)) {
+            continue
+        }
+        else {
+            outputString += i
+        }
+    }
+    return outputString
 }
 
