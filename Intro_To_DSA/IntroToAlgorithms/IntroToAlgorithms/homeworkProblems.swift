62829f767d69202d338a7fc3ca35e8c95f492485
edwardanchundia <edward.anchundia@gmail.com>
2016-10-18 10:38:05 -0400

    completed homework

--- Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
+++ Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
@@ -15,31 +15,73 @@ import Foundation
 
 
 func posZeroNeg(myArr: [Int]) -> (Int, Int, Int) {
-    return (0,0,0)
+    var negative = 0
+    var zero = 0
+    var positive = 0
+    
+    for num in myArr {
+        if num < 0 {
+            negative += 1
+        } else if num == 0 {
+            zero += 1
+        } else if num > 0 {
+            positive += 1
+        }
+    }
+    
+    
+    return (negative, zero, positive)
 }
 
 
 //Given an array of Ints, return the second smallest integer.  Assume every value is unique.  Return nil if the array has less than two elements
 
 func secondSmallest(myArr: [Int]) -> Int? {
-    return nil
+    let secondSmallestInt: Int
+    let orderedArr = myArr.sorted{ $0 < $1 }
+    
+    if orderedArr.count > 2 {
+        secondSmallestInt = orderedArr[1]
+    } else {
+        return nil
+    }
+    
+    return secondSmallestInt
 }
 
 //A number is prime if it is only evenly divisible by 1 and itself.  Write an algorithm that checks if an Int is prime
 
 func isPrime(x: Int) -> Bool {
+    
+    for num in 2..<x{
+        if x % num == 0{
+            return false
+        }
+    }
     return true
+    
 }
 
 //Write an anglorithm that removes all characters from a String matching an input character
 
 func removeCharacter(s: String, c: Character) -> String {
-    return ""
+    
+    let removedChar = s.characters.filter({ $0 != c })
+    
+    return String(removedChar)
 }
 
 //Write an algorithm that removes all characters from a String contained within an input array of characters
 
 func removeMultipleCharacters(s: String, arr: [Character]) -> String {
-    return ""
+    var newString: String = ""
+    
+    for char in s.characters {
+        if !arr.contains(char) {
+            newString.append(char)
+        }
+    }
+    
+    return newString
 }
 
