f60d4cbf6e64dfd1ded45ed012636ab27d9909ca
Sabrina Ip <kkip90@gmail.com>
2016-10-24 14:10:16 -0400

    weekone

--- Week One Review/DSAWeekOneReview.playground/Contents.swift
+++ Week One Review/DSAWeekOneReview.playground/Contents.swift
@@ -11,26 +11,78 @@ var str = "Hello, playground"
 //Big O Notation
 
 /*
-1) Write an algorithm with the following runtimes:
-
-a) O(1)
+ 1) Write an algorithm with the following runtimes:
  
-b) O(n)
- 
-c) O(nlog(n))
- 
-d) O(n^2)
- 
-e) O(n^3)
- 
-f) O(n^4)
-
-g) O(n^2 * log(n))
-*/
+ a) O(1)
+ */
+func a(arr: [Int]) -> Int {
+    return arr[0]
+}
+/*
+ b) O(n)
+ */
+func b(arr: [Int]) {
+    for i in arr {
+        print(i)
+    }
+}
+/*
+ c) O(nlog(n))
+ */
+func c(arr: [Int]) -> [Int] {
+    return arr.sorted(by: >)
+}
+/*
+ d) O(n^2)
+ */
+func d(arr: [Int]) {
+    for i in arr{
+        for j in 0..<arr.count{
+            print(i*j)
+        }
+    }
+}
+/*
+ e) O(n^3)
+ */
+func e(arr: [Int]) {
+    for i in arr {
+        for j in arr {
+            for k in arr {
+                print(i * j - k)
+            }
+        }
+    }
+}
+/*
+ f) O(n^4)
+ */
+func f(arr: [Int]) {
+    for i in arr {
+        for j in arr {
+            for k in arr {
+                for l in arr {
+                    print(l)
+                    print(i * j - k)
+                }
+            }
+        }
+    }
+}
+/*
+ g) O(n^2 * log(n))
+ */
+func g(arr: [[Int]]) -> [[Int]] {
+    var returnArr = [[Int]]()
+    for interiorArr in arr {
+        returnArr.append(interiorArr.sorted())
+    }
+    return returnArr
+}
 
 /*
-2) Identify the runtimes of the following algorithms
-*/
+ 2) Identify the runtimes of the following algorithms
+ */
 
 func problemA(myString: String) {
     for c in myString.characters {
@@ -39,19 +91,19 @@ func problemA(myString: String) {
         }
     }
 }
- 
-//Runtime: 
+
+//Runtime: O(n^2)
 
 func problemB(myArr: [[Int]]) {
     var otherArr = [[Int]]()
-    for i in myArr {
-        print(i[0])
-        otherArr.append(i)
-        print(myArr[i[0]].contains(3))
+    for i in myArr { // O(n)
+        print(i[0]) // O(1)
+        otherArr.append(i) // O(1)
+        print(myArr[i[0]].contains(3)) //O(m)
     }
 }
 
-//Runtime:
+//Runtime: O(n * m)
 
 func problemC(myArr: [Int]) -> Bool {
     if myArr.count < 10_000 {
@@ -61,19 +113,19 @@ func problemC(myArr: [Int]) -> Bool {
     }
 }
 
-//Runtime:
+//Runtime: O(1)
 
 func problemD(myArr: [Bool]) -> [Bool] {
     return myArr.map{!$0}
 }
 
-//Runtime:
+//Runtime: O(n)
 
 func problemE(myArr: [Int]) -> [Int] {
     return myArr.filter{$0 > 5}.map{$0 * 3}.sorted(by: <)
 }
 
-//Runtime:
+//Runtime: O(nlog(n))
 
 func problemF(myArr: Int) {
     for i in 0..<myArr {
@@ -81,26 +133,26 @@ func problemF(myArr: Int) {
     }
 }
 
-//Runtime:
+//Runtime: O(n)
 
 func problemG(myArr: [[[[[[String]]]]]]) {
     print(myArr[0][0][0][0][0].contains("hi!"))
 }
 
-//Runtime:
+//Runtime: O(n)
 
 func problemH(arrOne: [Int], arrTwo: [Int]) {
     var counter = 0
-    for numOne in arrOne {
-        for numTwo in arrTwo {
-            if arrOne.contains(numTwo) && arrTwo.contains(numOne) {
+    for numOne in arrOne { // O(n)
+        for numTwo in arrTwo { // O(n)
+            if arrOne.contains(numTwo) && arrTwo.contains(numOne) { // O(n) + O(n) => O(n)
                 counter += 1
             }
         }
     }
 }
 
-//Runtime:
+//Runtime: O(n^3)
 
 func problemI(isEnabled: Bool) {
     for _ in 0..<(isEnabled ? 10 : 1_000_000) {
@@ -108,18 +160,23 @@ func problemI(isEnabled: Bool) {
     }
 }
 
-//Runtime:
+//Runtime: O(1)
 
 //Arrays - the data structure
 //1) You have an array of 100 Strings (24 bytes each) at memory address 0ff3c0000.
 
 //a)What is the memory address of the 1st element?
+//0ff3c0000
 //b)What is the memory address of the 2nd element?
+//0ff3c0018
 //c)What is the memory address of the 4th element?
+//0ff3c0048
 //d)What is the memory address of the 14th element?
+//0ff3c0138
 //e)What is the memory address of the 52nd element?
+//0ff3c04c8
 //f)What is the memory address of the 58th element?
-
+//0ff3c0558
 
 
 //2) Identify and explain the runtimes for the following array operations:
@@ -127,37 +184,37 @@ func problemI(isEnabled: Bool) {
 var myArr = [13,41,3,13,13,12,12,1,9]
 
 //a)
-myArr.popLast()
+myArr.popLast() //constant
 
 //b)
-myArr.contains(1)
+myArr.contains(1) //linear
 
 //c
-myArr.index(of: 9)
+myArr.index(of: 9) // linear
 
 //d
-myArr.count
+myArr.count // constants
 
 //e
-myArr.insert(8, at: 0)
+myArr.insert(8, at: 0) // linear
 
 //f
-myArr.remove(at: 4)
+myArr.remove(at: 4) // linear
 
 //g
-myArr.reverse()
+myArr.reverse() //linear
 
 //h
-let h = myArr.sorted(by: >)
+let h = myArr.sorted(by: >) // nlog(n)
 
 //i
-let i = myArr.map{$0 * 2}
+let i = myArr.map{$0 * 2} // linear
 
 //j
-let j = myArr.filter{$0>0}
+let j = myArr.filter{$0>0} // linear
 
 //k
-let k = myArr.reduce(0, +)
+let k = myArr.reduce(0, +) // linear
 
 
 //3) Given the array below write code that does the following:
@@ -170,7 +227,24 @@ let inputArray: [[Int]] = [
 ]
 
 //a) Print out each element starting with the first row and ending with the last row
+
+for interiorArray in inputArray {
+    for num in interiorArray {
+        print(num, terminator: " ")
+    }
+}
+print()
+
 //b) Print out each element starting with the first column and ending with the last column
+
+for i in 0..<inputArray.count {
+    for interiorArray in inputArray {
+        print(interiorArray[i], terminator: " ")
+    }
+    print(terminator: "  ")
+}
+print()
+
 //c) Print out each diagonal
 //d) Print out only numbers on the border
 //e) Print out only numbers not on the border
@@ -178,20 +252,19 @@ let inputArray: [[Int]] = [
 
 //4) Sudoku
 /*
-Rules: The is a 9x9 gird where the objective is to place the numbers 1 to 9 in the empty squares so that each row, each column and each 3x3 box contains the same number only once.
-
-Sudoku Example: https://github.com/C4Q/AC-DSA/blob/master/Arrays/Images/sudokuExamples.png
+ Rules: The is a 9x9 gird where the objective is to place the numbers 1 to 9 in the empty squares so that each row, each column and each 3x3 box contains the same number only once.
  
-Exercise: Write a function that will check whether or not a fully filled sudoku board is a valid solution.
-
-Input: 2D Array of Ints ranging from 1-9
-Output: True or false
-func checkSudokuBoard(sudokuBoard:[[Int]]) -> Bool {
-    return true
-}
-
-*/
-
+ Sudoku Example: https://github.com/C4Q/AC-DSA/blob/master/Arrays/Images/sudokuExamples.png
+ 
+ Exercise: Write a function that will check whether or not a fully filled sudoku board is a valid solution.
+ 
+ Input: 2D Array of Ints ranging from 1-9
+ Output: True or false
+ func checkSudokuBoard(sudokuBoard:[[Int]]) -> Bool {
+ return true
+ }
+ 
+ */
 
 
 
