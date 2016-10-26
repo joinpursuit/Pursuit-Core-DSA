64c44cb2ac93545dc9050a8357b038f1cdbb00ea
Tom Seymour <tomeseymour44@gmail.com>
2016-10-26 07:19:37 -0400

    pre test

--- Linked Lists/LinkedListsHomeworkTwo/LinkedListsHomeworkTwo/DoublyLinkedList.swift
+++ Linked Lists/LinkedListsHomeworkTwo/LinkedListsHomeworkTwo/DoublyLinkedList.swift
@@ -18,21 +18,129 @@ class Node<T> {
 class LinkedList<T: Equatable> {
     var head  = Node<T>()
     
-    
     func printAllKeys() {
-        
+        if head.key == nil {
+            print("No Elements In List")
+        } else {
+            var currentNode = head
+            while currentNode.next != nil {
+                print(currentNode.key)
+                currentNode = currentNode.next!
+            }
+            print(currentNode.key!)
+        }
     }
-    var count: Int {return 0}
     
-    func append(element newKey: T) {}
+    var count: Int {
+        if head.key == nil {
+        return 0
+    } else {
+        var counter = 1
+        var currentNode = head
+        while currentNode.next != nil {
+            counter += 1
+            currentNode = currentNode.next!
+        }
+        return counter
+        }
+    }
     
-    func getElement(at Index: Int) -> Node<T>? {return nil}
+    func append(element newKey: T) {
+        if head.key == nil {
+            head.key = newKey
+        } else {
+            var currentNode = head
+            while currentNode.next != nil {
+                currentNode = currentNode.next!
+            }
+            let newNode = Node<T>()
+            newNode.key = newKey
+            newNode.previous = currentNode
+            currentNode.next = newNode
+        }
+    }
     
-    func insert(_ key: T, at index: Int) {}
+    func getElement(at index: Int) -> Node<T>? {
+        if (index > self.count - 1 || index < 0 || head.key == nil) {
+            return nil
+        } else {
+            var count = 0
+            var currentNode = head
+            while currentNode.next != nil {
+                if count == index {
+                    return currentNode
+                }
+                currentNode = currentNode.next!
+                count += 1
+            }
+            return nil
+        }
+    }
     
-    func contains(element targetKey: T) -> Bool {return false}
+    func contains(element targetKey: T) -> Bool {
+        if head.key == targetKey {
+            return true
+        }
+        var currentNode = head
+        while currentNode.next != nil {
+            currentNode = currentNode.next!
+            if currentNode.key == targetKey {
+                return true
+            }
+        }
+        return false
+    }
     
-    func remove(at index: Int) {}
+    func insert(_ key: T, at index: Int) {
+        if (index > self.count - 1 || index < 0 || head.key == nil) {
+            return
+        } else {
+            var count = 1
+            var currentNode = head
+            while currentNode.next != nil {
+                if count == index {
+                    let newNode = Node<T>()
+                    newNode.key = key
+                    newNode.next = currentNode.next
+                    newNode.previous = currentNode
+                    currentNode.next = newNode
+                    break
+                }
+                currentNode = currentNode.next!
+                count += 1
+            }
+        }
+    }
     
-    func reverse() {}
+    func remove(at index: Int) {
+        if (index > self.count - 1 || index < 0 || head.key == nil) {
+            return
+        } else {
+            var count = 0
+            var currentNode = head
+            while currentNode.next != nil {
+                if count == index - 1 {
+                    currentNode.next = currentNode.next?.next
+                    currentNode.next?.previous = currentNode
+                    break
+                }
+                currentNode = currentNode.next!
+                count += 1
+            }
+        }
+    }
+    
+    func reverse() {
+        var previousNode: Node<T>? = nil
+        var currentNode: Node<T>? = head
+        while currentNode != nil {
+            let temp = currentNode!.next
+            currentNode?.previous = currentNode?.next
+            currentNode!.next = previousNode
+            previousNode = currentNode
+            currentNode = temp
+        }
+        head = previousNode!
+    }
+
 }
