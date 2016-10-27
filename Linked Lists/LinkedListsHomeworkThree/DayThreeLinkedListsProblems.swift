9e09e84cc36b61e1833234089a8193861ffb05b7
Eashir Arafat <eashirarafat@gmail.com>
2016-10-27 10:29:35 -0400

    2 props completed

--- Linked Lists/LinkedListsHomeworkThree/DayThreeLinkedListsProblems.swift
+++ Linked Lists/LinkedListsHomeworkThree/DayThreeLinkedListsProblems.swift
@@ -32,7 +32,18 @@ class LinkedList<T: Equatable> {
     
     //add link item
     func append(element key: T) {
-  
+        let newNode = Node()
+    
+        if let tailNode = tail {
+            newNode.previous = tailNode
+            tailNode.next = newNode
+        }
+            
+        else {
+            head = newNode
+        }
+        
+        tail = newNode
     }
     
     //print all keys for the class
@@ -42,7 +53,18 @@ class LinkedList<T: Equatable> {
     
     //obtain link at a specific index
     func getElement(at index: Int) ->Node<T>! {
-        return Node<T>()
+        if index >= 0 {
+            var node = head
+            var i = index
+            // 2
+            while node != nil {
+                if i == 0 { return node }
+                i -= 1
+                node = node!.next
+            }
+        }
+        // 3
+        return nil
     }
     
     //insert at specific index
@@ -52,6 +74,25 @@ class LinkedList<T: Equatable> {
     
     //remove at specific index
     func remove(at index: Int) {
-
+        let prev = node.previous
+        let next = node.next
+        
+        if let prev = prev {
+            prev.next = next // 1
+        } else {
+            head = next // 2
+        }
+        next?.previous = prev // 3
+        
+        if next == nil {
+            tail = prev // 4
+        }
+        
+        // 5
+        node.previous = nil 
+        node.next = nil
+        
+        // 6
+        return node.value
     }
 }
