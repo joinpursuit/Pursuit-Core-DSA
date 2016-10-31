3ab0e68af28021c76576aac2e240b2926113dddd
defintuition <ahspadafora@gmail.com>
2016-10-31 14:21:16 -0400

    excercises

--- Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
+++ Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
@@ -13,21 +13,83 @@ import Foundation
 //The second element is the number of zeros 
 //The third element is the number of positive values.
 
-
 func posZeroNeg(myArr: [Int]) -> (Int, Int, Int) {
-    return (0,0,0)
+    
+    var numOfNegValues: Int = 0
+    var numOfZeros: Int = 0
+    var numOfPositiveValues: Int = 0
+    
+    for element in myArr {
+        if element < 0 {
+            numOfNegValues += 1
+        }
+        
+        if element == 0 {
+            numOfZeros += 1
+        }
+        
+        if element > 0 {
+            numOfPositiveValues += 1
+        }
+        
+    }
+    
+    let tuple = (numOfNegValues, numOfZeros,numOfPositiveValues)
+    
+    print(tuple)
+    return tuple
+    
 }
 
+//posZeroNeg(myArr: [-2,-3,-4,-5,-6,-7,-9])
 
 //Given an array of Ints, return the second smallest integer.  Assume every value is unique.  Return nil if the array has less than two elements
 
 func secondSmallest(myArr: [Int]) -> Int? {
-    return nil
+    var myArrCopy = myArr.sorted {
+        return $0 < $1
+    }
+    
+    if myArr.count < 2 {
+        return nil
+    } else {
+        myArrCopy.remove(at: 0)
+        return myArrCopy[0]
+    }
+    
 }
+
+//secondSmallest(myArr: [59,12,4,16,23,85])
+//secondSmallest(myArr: [0])
 
 //A number is prime if it is only evenly divisible by 1 and itself.  Write an algorithm that checks if an Int is prime
 
+
 func isPrime(x: Int) -> Bool {
+    switch x {
+    case 1:
+        return true
+    case 2:
+        return true
+    case 3:
+        return true
+    case 5:
+        return true
+    case 7:
+        return true
+    default:
+        break
+    }
+    
+    if x % 2 == 0 {
+        return false
+    } else if x % 3 == 0 {
+        return false
+    } else if x % 5 == 0 {
+        return false
+    } else if x % 7 == 0 {
+        return false
+    }
     return true
 }
 
