35c9844ce05dfe26eb72b4b833ceed40f78a44f9
Ben Stone <benjamin.h.stone@gmail.com>
2016-10-31 15:54:36 -0400

    updated stacks playground

--- Stacks/Stacks.playground/Contents.swift
+++ Stacks/Stacks.playground/Contents.swift
@@ -1,27 +1,42 @@
-//: Playground - noun: a place where people can play
+import UIKit
 
+var str = "Hello, playground"
 class StackWithArr<T> {
     private var arr = Array<T>()
     
-    func pop(){
-        
+    func pop() -> T? {
+        //      return arr.popLast()
+        guard arr.count != 0 else {
+            return nil
+        }
+        return arr.remove(at: arr.count-1)
     }
     
-    func push() {
-        
+    func push(newElement: T) {
+        arr.append(newElement)
     }
     
-    func peek() {
-
+    func peek() -> T? {
+        guard arr.count != 0 else {
+            return nil
+        }
+        return arr[arr.count-1]
     }
     
-    func isEmpty() {
-        
+    func isEmpty() -> Bool {
+        //return arr.isEmpty
+        return arr.count == 0
     }
 }
 
 let myStack = StackWithArr<Int>()
-
+print("Stack with arr output:")
+myStack.push(newElement: 9)
+myStack.push(newElement: 39)
+print(myStack.peek())
+myStack.pop()
+print(myStack.peek())
+print(myStack.isEmpty())
 
 class Node<T> {
     var key: T!
@@ -32,43 +47,43 @@ class LinkedList<T> {
     var head = Node<T>()
 }
 
-// H -> 2 -> 3
-// new -> H -> 2 -> 3
-
-
 class StackWithLL<T> {
     private var list = LinkedList<T>()
-    //pop
     func pop() -> T? {
-        guard list.head.next != nil else {
-            list.head.key = nil
-            return nil
+        let oldHead = list.head.key
+        if let next = list.head.next {
+            list.head = next
+        } else {
+            list.head = Node<T>()
         }
-        let oldKey = list.head.key
-        list.head = list.head.next!
-        return oldKey
+        return oldHead
     }
-    //push
-    func push(element: T) {
-        if list.head.key == nil {
-            list.head.key = element
+    func push(newElement: T) {
+        guard list.head.key != nil else {
+            list.head.key = newElement
             return
         }
         let newNode = Node<T>()
-        newNode.key = element
+        newNode.key = newElement
         newNode.next = list.head
         list.head = newNode
     }
-    
-    //peek
     func peek() -> T? {
         return list.head.key
     }
-    
-    //isEmpty
     func isEmpty() -> Bool {
         return list.head.key == nil
     }
 }
 
 let myStackLL = StackWithLL<Int>()
+print("Stack with LL output:")
+myStackLL.push(newElement: 9)
+myStackLL.push(newElement: 39)
+print(myStackLL.peek())
+myStackLL.pop()
+print(myStackLL.peek())
+print(myStackLL.isEmpty())
+myStackLL.pop()
+print(myStackLL.isEmpty())
+myStackLL.pop()
