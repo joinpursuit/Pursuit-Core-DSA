81abd508902de5530276f248fb2de5682d91a4a4
Madushani Lekam Wasam Liyanage <Shashi@Shashis-MacBook.home>
2016-11-01 00:16:09 -0400

    Stack HW

--- Stacks/StackHomeworkProblems/StackHomeworkProblems/StackProblems.swift
+++ Stacks/StackHomeworkProblems/StackHomeworkProblems/StackProblems.swift
@@ -12,8 +12,22 @@ import Foundation
 //Problem One:
 //Find the largest integer in a Stack of Ints
 
+//this will make our stack empty. so we'll have to re populate the stack
 func largest(stack: Stack<Int>) -> Int {
-    return 0
+    var arr = Array<Int>()
+    
+    while !stack.isEmpty() {
+        if let poppedInt = stack.pop() {
+            arr.append(poppedInt)
+        }
+    }
+    var largestInt = arr[0]
+    for anInt in arr {
+        if largestInt < anInt {
+            largestInt = anInt
+        }
+    }
+    return largestInt
 }
 
 
@@ -21,7 +35,20 @@ func largest(stack: Stack<Int>) -> Int {
 //Find the sum of a Stack of Ints
 
 func sum(stack: Stack<Int>) -> Int {
-    return 0
+    var arr = Array<Int>()
+    
+    while !stack.isEmpty() {
+        if let poppedInt = stack.pop() {
+            arr.append(poppedInt)
+        }
+    }
+    
+    var sum = 0
+    for anInt in arr {
+        sum += anInt
+        
+    }
+    return sum
 }
 
 //Problem Three:
@@ -33,7 +60,7 @@ func sum(stack: Stack<Int>) -> Int {
  2
  9
  3
-*/
+ */
 
 //Sample output:
 /*
@@ -41,10 +68,20 @@ func sum(stack: Stack<Int>) -> Int {
  9
  2
  4
-*/
+ */
 
 func reverse<T>(stack: Stack<T>) -> Stack<T> {
-    return Stack<T>()
+    
+    let reversedStack = Stack<T>()
+    
+    while !stack.isEmpty() {
+        if let poppedT = stack.pop() {
+            reversedStack.push(element: poppedT)
+            
+        }
+        
+    }
+    return reversedStack
 }
 
 
@@ -52,7 +89,28 @@ func reverse<T>(stack: Stack<T>) -> Stack<T> {
 //Determine if two stacks are equal
 
 func equalStacks<T: Equatable>(stackOne: Stack<T>, stackTwo: Stack<T>) -> Bool {
-    return false
+    
+    var arr1 = Array<T>()
+    var arr2 = Array<T>()
+    
+    while !stackOne.isEmpty() {
+        if let poppedT = stackOne.pop() {
+            arr1.append(poppedT)
+        }
+    }
+    
+    while !stackTwo.isEmpty() {
+        if let poppedT = stackTwo.pop() {
+            arr2.append(poppedT)
+        }
+    }
+    
+    if arr1 == arr2 {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 
@@ -60,7 +118,22 @@ func equalStacks<T: Equatable>(stackOne: Stack<T>, stackTwo: Stack<T>) -> Bool {
 //Write a function that pushes a new element to the bottom of a Stack
 
 func pushBottom<T>(stack: Stack<T>, newElement: T) -> Stack<T> {
-    return Stack<T>()
+    var arr = Array<T>()
+    let newStack = Stack<T>()
+    
+    while !stack.isEmpty() {
+        if let poppedT = stack.pop() {
+            arr.append(poppedT)
+        }
+    }
+    
+    arr.append(newElement)
+    for i in 0..<arr.count {
+        newStack.push(element: arr[arr.count-1-i])
+    }
+    
+    
+    return newStack
 }
 
 //Problem Six:
@@ -73,17 +146,51 @@ func pushBottom<T>(stack: Stack<T>, newElement: T) -> Stack<T> {
 //Sample output: false
 
 //Sample input: (()((())()))
-//Sample output: false
+//Sample output: true
 
 
 func isBalanced(str: String) -> Bool {
-    return false
+    let stringStack = Stack<String>()
+    var counter1 = 0
+    var counter2 = 0
+    
+    for aChar in Array(str.characters){
+        stringStack.push(element: String(aChar))
+    }
+    while !stringStack.isEmpty() {
+        let el = stringStack.pop()
+        if  el == "(" {
+            counter1 += 1
+        }
+        else if el == ")" {
+            counter2 += 1
+        }
+    }
+    if counter1 == counter2 {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 //Bonus: Problem Seven:
 //Use a stack to convert a number in decimal to binary
 
 func convertToBinary(_ num: Int) -> String {
-    return ""
+    let newStack = Stack<String>()
+    var myNum = num
+    var str = ""
+    
+    while myNum > 0 {
+        newStack.push(element: String(myNum % 2))
+        myNum = myNum / 2
+    }
+    while !newStack.isEmpty() {
+        let binaryStr = newStack.pop()
+        str += binaryStr!
+    }
+    
+    return str
 }
 
--- Stacks/Stacks.playground/Contents.swift
+++ Stacks/Stacks.playground/Contents.swift
@@ -1,26 +1,42 @@
 //: Playground - noun: a place where people can play
 
+/*
 class StackWithArr<T> {
     private var arr = Array<T>()
     
-    func pop(){
+    func pop() -> T? {
+       // return arr.popLast()
+        guard arr.count != 0 else {
+            return nil
+        }
+        let last = arr[arr.count - 1]
+        arr.remove(at: arr.count - 1)
+        return last
+    }
+    
+    func push(newElement: T) {
+        arr.append(newElement)
+    }
+    
+    func peek() -> T? {
+        guard  arr.count != 0 else {
+            return nil
+        }
+        return arr[arr.count-1]
         
     }
     
-    func push() {
-        
-    }
-    
-    func peek() {
-
-    }
-    
-    func isEmpty() {
-        
+    func isEmpty() -> Bool {
+        return arr.isEmpty
     }
 }
 
 let myStack = StackWithArr<Int>()
+myStack.push(newElement: 67)
+myStack.push(newElement: 87)
+print(myStack.peek())
+print(myStack.pop())
+*/
 
 
 class Node<T> {
@@ -39,23 +55,45 @@ class LinkedList<T> {
 class StackWithLL<T> {
     private var list = LinkedList<T>()
     //pop
+//    func pop() -> T? {
+//        guard list.head.next != nil else {
+//            list.head.key = nil
+//            return nil
+//        }
+//        let oldKey = list.head.key
+//        list.head = list.head.next!
+//        return oldKey
+//    }
+    
     func pop() -> T? {
-        guard list.head.next != nil else {
-            list.head.key = nil
-            return nil
+        let oldHead = list.head.key
+        if let next = list.head.next {
+            list.head = next
+        } else {
+            list.head = Node<T>()
         }
-        let oldKey = list.head.key
-        list.head = list.head.next!
-        return oldKey
+        return oldHead
     }
+    
     //push
-    func push(element: T) {
-        if list.head.key == nil {
-            list.head.key = element
+//    func push(element: T) {
+//        if list.head.key == nil {
+//            list.head.key = element
+//            return
+//        }
+//        let newNode = Node<T>()
+//        newNode.key = element
+//        newNode.next = list.head
+//        list.head = newNode
+//    }
+    
+    func push(newElement: T) {
+        guard list.head.key != nil else {
+            list.head.key = newElement
             return
         }
         let newNode = Node<T>()
-        newNode.key = element
+        newNode.key = newElement
         newNode.next = list.head
         list.head = newNode
     }
@@ -72,3 +110,9 @@ class StackWithLL<T> {
 }
 
 let myStackLL = StackWithLL<Int>()
+
+let myStack = StackWithLL<Int>()
+myStack.push(newElement: 67)
+myStack.push(newElement: 87)
+print(myStack.peek())
+print(myStack.pop())
