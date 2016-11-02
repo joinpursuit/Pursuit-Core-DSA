b112c8a44ada49ea98b8b908ae2af8c5bef48947
Eric Chang <echang25@hotmail.com>
2016-11-02 11:49:46 -0400

    queue hw

--- Queues/QueuesHWProblems/QueuesHWProblems/QueueProblems.swift
+++ Queues/QueuesHWProblems/QueuesHWProblems/QueueProblems.swift
@@ -14,30 +14,81 @@ import Foundation
 
 //Find the sum of a queue
 func sum(q: Queue<Int>) -> Int? {
-    return nil
+  guard !q.isEmpty() else { return nil }
+  
+  var sum = 0
+  while !q.isEmpty(){
+    let num = q.deQueue()
+    sum += num!
+  }
+  return sum
 }
 
 //Find the smallest element in a queue
 func smallest<T:Comparable>(q: Queue<T>) -> T? {
-    return nil
+  guard !q.isEmpty() else { return nil }
+  
+  var small = q.peek()!
+  
+  while !q.isEmpty() {
+    let isThisSmaller = q.deQueue()
+    if isThisSmaller! <= small {
+      small = isThisSmaller!
+    }
+  }
+  
+  return small
 }
 
 //Find out whether or not a queue is in sorted order from smallest to biggest
 //True example:   (Back) 9 - 6 - 2 - 1 (Front)
 //False example   (Back) 4 - 19 - 134 200 (Front)
-func isSorted<T: Comparable>(q: Queue<T>) -> Bool {
-    return false
+func isSorted<T: Comparable>(q: Queue<T>) -> Bool? {
+  guard !q.isEmpty() else { return nil }
+  
+  var arr = [T]()
+  
+  while !q.isEmpty() {
+    let next = q.deQueue()
+    arr.append(next!)
+  }
+  
+  guard arr == arr.sorted() else { return false }
+  return true
 }
 
 //Return a reversed queue.
 func reverse<T>(q: Queue<T>) -> Queue<T> {
-    return Queue<T>()
+  
+  let stack = Stack<T>()
+  let newQ = Queue<T>()
+  
+  while !q.isEmpty() {
+    let next = q.deQueue()
+    stack.push(element: next!)
+  }
+  
+  while !stack.isEmpty() {
+    let next = stack.pop()!
+    newQ.enQueue(newElement: next)
+  }
+  
+  return newQ
 }
 
 //Determine if two queues are equal.
 
 func areEqual<T: Equatable>(qOne: Queue<T>, qTwo: Queue<T>) -> Bool {
-    return false
+  guard !qOne.isEmpty() && !qTwo.isEmpty() else { return true }
+  if qOne === qTwo { return true }
+  
+  while !qOne.isEmpty() || !qTwo.isEmpty() {
+    if qOne.isEmpty() || qTwo.isEmpty() { return false }
+    let one = qOne.deQueue()
+    let two = qTwo.deQueue()
+    guard one == two else { return false }
+  }
+  return true
 }
 
 //Bonus: Hot Potato
--- Queues/QueuesPlaygroundFromClass.playground/Contents.swift
+++ Queues/QueuesPlaygroundFromClass.playground/Contents.swift
@@ -7,7 +7,7 @@ class Node<T> {
     var next: Node<T>?
 }
 
-class LinekdList<T> {
+class LinkedList<T> {
     var head: Node<T>
     var tail: Node<T>
     init() {
@@ -17,34 +17,24 @@ class LinekdList<T> {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 class Queue<T> {
-    private var list = LinekdList<T>()
-    
+    private var list = LinkedList<T>()
+  
+  
     func enQueue(newElement: T) {
         if list.head.key == nil {
             list.head.key = newElement
             return
-        } else {
+        }
+        else {
             let newNode = Node<T>()
             newNode.key = newElement
             list.tail.next = newNode
             list.tail = newNode
         }
     }
-    
+  
+  
     func deQueue() -> T? {
         if let oldHeadKey = list.head.key {
             if let nextNode = list.head.next {
@@ -57,15 +47,80 @@ class Queue<T> {
             return nil
         }
     }
-    
+  
+  
     func isEmpty() -> Bool {
         return list.head.key == nil
     }
-    
+  
+  
     func peek() -> T? {
         return list.head.key
     }
 }
- */
+
+
+/*----- Stacks and Queues -----*/
+/*----- Stacks and Queues -----*/
+
+class Stack<T> {
+  
+  private var arr = Array<T>()
+  
+  func pop() -> T? {
+    return arr.popLast()
+  }
+  
+  func push(element: T) {
+    arr.append(element)
+  }
+  
+  func peek() -> T? {
+    guard arr.count > 0 else {
+      return nil
+    }
+    return arr[arr.count - 1]
+  }
+  
+  func isEmpty() -> Bool {
+    return arr.count == 0
+  }
+}
+
+
+class QueueTwoStacks<T> {
+  private var enQueueStack = Stack<T>()
+  private var deQueueStack = Stack<T>()
+  
+  
+  func enQueue(newElement: T) {
+    while !(deQueueStack.isEmpty()) {
+      enQueueStack.push(element: deQueueStack.pop()!)
+    }
+    enQueueStack.push(element: newElement)
+  }
+  
+  
+  func deQueue() -> T? {
+    while !(enQueueStack.isEmpty()) {
+      deQueueStack.push(element: enQueueStack.pop()!)
+    }
+    return deQueueStack.pop()
+  }
+  
+  
+  func peek() -> T? {
+    while !(enQueueStack.isEmpty()) {
+      deQueueStack.push(element: enQueueStack.pop()!)
+    }
+    return deQueueStack.peek()
+  }
+  
+  
+  func isEmpty() -> Bool {
+    return deQueueStack.isEmpty() && enQueueStack.isEmpty()
+  }
+  
+}
 
 
